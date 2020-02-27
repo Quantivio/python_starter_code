dcaee9faaeaaa8bf4ca4973a314557003b83875f
Python Hub <pythonhub.py@gmail.com>
2020-02-27 20:56:20 +0530

    Added Complement and Shift operator example code.

---
+++ complement_and_shift.py
@@ -0,0 +1,16 @@
+# Complement operator (~):cls
+
+print(~12)  # Printing the complement of 12
+
+# ~12 = -13
+
+""" Internal working of the complement operator"""
+""" Manual calculation"""
+""" 2's Complement is the method used to convert negative numbers to positive numbers and store them in the computer."""
+
+#   Binary of 12           = 00001100
+#   Complement of 00001100 = 11110011
+
+#   Binary of 13                = 00001101
+#   Complement of 00001101      = 11110010
+#   2's Complement of 11110010  = 11110011
--- float_functions.py
+++ float_functions.py
@@ -1,13 +1,13 @@
 floating_value = 10.5  # Declaring a floating value.
 
-# Implementation of function as_integer_ratio()
+# Implementation of function as_integer_ratio():
 
 """ Returns a pair of integer values whose ratio is equal to 
 the corresponding integer or float value with positive denominator."""
 
 print(floating_value.as_integer_ratio())
 
-# Implementation of function hex()
+# Implementation of function hex():
 
 """ Converts the floating point value into hexadecimal value."""
 
