742e8f3575fd68b3b147fb6f39617a8e31f9c5f5
Python Hub <pythonhub.py@gmail.com>
2020-02-29 01:34:16 +0530

    Number conversion with format specifiers.

---
+++ number_conversion_with_format_specifiers.py
@@ -0,0 +1,42 @@
+""" In python format specifiers like %i can be used"""
+""" Using the format specifiers number conversions can be made."""
+
+"""Format specifiers:
+%i => Integer or Decimal 
+%s => String
+%c => Character
+%f => Float
+%x => Hexadecimal
+%o => Octal
+"""
+
+# There is no format specifier for binary.
+
+number = 20  # Decimal a number.
+
+# Conversion of decimal to octal, hexadecimal with format specifiers:
+
+print("Ocatl: %o\nHexadecimal: %x" % (number, number))
+
+
+# Conversion of binary to decimal, octal, hexadecimal with format specifiers:
+
+binary = 0b1001
+
+print("Decimal: %i\nOcatl: %o\nHexadecimal: %x" % (binary, binary, binary))
+
+
+# Conversion of octal to decimal, octal, hexadecimal with format specifiers:
+
+octal = 0o1001
+
+print("Decimal: %i\nHexadecimal: %x" % (octal, octal))
+
+
+# Conversion of hexadecimal to decimal, octal,  with format specifiers:
+
+hexadecimal = 0x100
+
+print("Decimal: %i\Octal: %x" % (hexadecimal, hexadecimal))
+
+
