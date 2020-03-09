d0acca19b935a182e6de955db601d4d805ec049a
Python Hub <pythonhub.py@gmail.com>
2020-03-10 01:48:30 +0530

    Started string functions

---
+++ string_functions.py
@@ -0,0 +1,67 @@
+# String Functions
+
+# There are various built-in methods that can be implemented on strings.
+
+# capitalize():
+
+""" capitalize() changes the first letter of the string into capital letter."""
+
+string_ = "smallletter"  # Declaring a variable with string value.
+
+print(string_.capitalize())
+
+# casefold():
+
+""" casefold() is an aggressive lower() method which converts string to casefolded string."""
+
+caps_string = "APPLE"
+
+print(caps_string.casefold())
+
+# center():
+
+""" center() is used to center a string at the center of certain charcater that are repeating on left and right."""
+
+""" Synatx: variable_name.center(width, fillchar) """
+
+"""
+Takes two arguments:
+
+1. width    => It is the complete length of the string that the resulting string must have.
+2. fillchar => Which character should be repeated on the left and right.
+
+NOTE : If fillchar is not specified then the default character is space.
+"""
+
+print(caps_string.center(24, "g"))
+
+# count()
+
+""" count() is used to count the occurance of a character or a substring in the original string."""
+
+print(string_.count('l'))
+
+# encode()
+
+""" encode() returns an encoded version of the string."""
+
+""" Syntax: variable_name.encode(encoding, errors) """
+
+""" 
+Takes two arguments:
+
+1. encoding => Any supported encoding format in python.
+2. errors   => Response when encoding fails.
+
+"""
+
+""" 
+Errors:
+strict              => default response which raises a UnicodeDecodeError exception on failure.
+ignore              => ignores the unencodable unicode from the result
+replace             => replaces the unencodable unicode to a question mark ?
+xmlcharrefreplace   => inserts XML character reference instead of unencodable unicode
+backslashreplace    => inserts a \\uNNNN espace sequence instead of unencodable unicode
+namereplace         => inserts a \N{...} escape sequence instead of unencodable unicode
+
+"""
