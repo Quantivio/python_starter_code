164ebb20a0f889ace4f14e7e45dd7c57eae7ca65
Python Hub <pythonhub.py@gmail.com>
2020-03-12 22:38:11 +0530

    f-string method added

---
+++ 9.5.2-f-string.py
@@ -0,0 +1,15 @@
+# f-string
+
+""" f-string is a formatting method introduced in python 3.6."""
+""" f-string is more efficient that formatting with .format() and format specifiers."""
+""" 
+Syntax: f"hello {}"
+Values are to be placed inside {} paranthesis.
+"""
+
+
+age = 20  # Declaring a integer value.
+
+f_string = f"Hello I am {age} old"  # Declaring a formatted string with the help of f-string.
+
+print(f_string)
