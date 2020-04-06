3bea6807f00ed52bd7a470646efde4c0b2973e3e
Python Hub <pythonhub.py@gmail.com>
2020-04-06 21:36:28 +0530

    Added list

---
+++ 9.5.4-list_.py
@@ -0,0 +1,16 @@
+# list
+
+""" 
+List is an mutable data structure in python.
+List can contain all types of data types.
+List can contain duplicate elements.
+list is an inbuilt class in python
+"""
+
+# Declaring a list:
+
+list_variable = [] # Declaring an empty list
+
+list_variable_1 = list() # Declaring a list with the help of inbuilt function.
+
+list_one = [1, 2, 3.0, "a", "Hello", 2+3j] # List containing all data types.
