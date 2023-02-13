a990488faa432aaac326e25d733bd7e28298f6b1
Vetrichelvan <pythonhub.py@gmail.com>
2023-02-13 20:25:28 +0530

    Modified tuples with better code and comments

---
+++ 15. tuples/tuples.py
@@ -0,0 +1,19 @@
+# Tuples
+
+## Tuples are immutable ordered collections of elements that can be of different data types.
+
+## A tuple can be indexed, sliced and iterated over.
+
+# Declaring an empty tuple
+empty_tuple = ()
+
+# Declaring a tuple using the built-in `tuple()` function
+empty_tuple_ = tuple()
+
+# Declaring a tuple with elements of different data types
+mixed_data_types_tuple = (1, 2, 3.5, "wow")
+
+# A tuple can be declared very simply without brackets
+tuple_ = 1, 2, 3
+
+print("The type of the tuple is:", type(tuple_))
