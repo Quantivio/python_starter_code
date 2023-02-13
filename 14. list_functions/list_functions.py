8421316e4792d698cd4864132623bb03285418c4
Vetrichelvan <pythonhub.py@gmail.com>
2023-02-13 20:22:56 +0530

    Modified list function with better code and comments

--- 14. list_functions/list_functions.py
+++ 14. list_functions/list_functions.py
@@ -1,129 +1,116 @@
 # List Functions
 
-"""
-
-1. append():
-append() function will add element to the end of a list.
-
-"""
+# 1. append()
+# append() function adds an element to the end of a list.
 
 a = [1, 2, 3]  # Declaring a list.
 
 a.append(4)
 
-print(a)  # After appending print the list.
+print("After appending:", a)  # After appending print the list.
 
-""" 
-2. clear():
-clear() function clears all the elements in a list and will make it empty.
-"""
+# 2. clear()
+# clear() function clears all the elements in a list and makes it empty.
 
-a: list[str | int] = ["a", 1, 2, 4.0]
+a = ["a", 1, 2, 4.0]
 
-print(a)  # Printing the list before clearing.
+print("Before clearing:", a)  # Printing the list before clearing.
 
 a.clear()
 
-print(a)  # Returns an empty list since the list is cleared.
+print("After clearing:", a)  # Returns an empty list since the list is cleared.
 
-""" 
-3. count() and index():
-count() and index() works as same as the string functions
-Refer: string_functions.py
-"""
+# 3. count() and index()
+# count() and index() functions work in the same way as their string counterparts.
+# For more information, refer to the file string_functions.py
 
-"""
-4. copy():
-copy() makes a shallow coppy of the list.
-Shallow copy means any changes made to the copied list will not affect the original list.
-"""
+# 4. copy()
+# copy() makes a shallow copy of a list.
+# Shallow copy means that any changes made to the copied list will not affect the original list.
+
 a = [1, 2, 3]
 
 b = a.copy()  # Making a shallow copy of the list 'a'.
 
 # Printing a and b before changing values.
 
-print(a)
-print(b)
+print("Before changes:", a)
+print("Before changes:", b)
 
-b.remove(1)  # Changing a value in b.and
+b.remove(1)  # Changing a value in b.
 
 # Printing after making changes.
 
-print(a)
-print(b)
+print("After changes:", a)
+print("After changes:", b)
 
-"""
-5. insert()
-insert() is used to insert a element in the desired position or index in a list.
-Syntax:
-list.insert(index, object)
-"""
+# 5. insert()
+# insert() is used to insert an element at the desired position or index in a list.
+# Syntax: list.insert(index, object)
 
 a = [1, 2, 3]
 
 a.insert(0, 5)  # Inserting 5 at the index position 0.
 
-print(a)
+print("After inserting:", a)
 
-"""
-6. extend()
-extend() is used to add a set of iterables to the end of a list.
-"""
+# 6. extend()
+# extend() adds a set of iterables to the end of a list.
 
 a = [1, 2, 3]
 
 a.extend([4, 5, 6, 100, 200])
 
-print(a)
+print("After extending:", a)
 
-"""
-7. pop()
-pop() is used to remove an element from the list based on the index position.
-when no arguments are passed pop() removes the last element in a list.
-"""
+# 7. pop()
+# pop() removes an element from a list based on its index position.
+# If no arguments are passed, pop() removes the last element in a list and returns the removed value.
 
 a = [1, 2, 3]
 
-print(a.pop())  # Since no argument is passed pop removes the last element. pop() returns the removed value.
+print("Removed element:", a.pop())  # Since no argument is passed, pop removes the last element.
 
-print(a)
+print("After popping:", a)
 
 a = [1, 2, 3, 4]
 
-print(a.pop(1))  # Passing the index value as 1.
+print("Removed element:", a.pop(1))  # Passing the index value as 1.
 
-print(a)
+print("After popping:", a)
 
-"""8. remove() remove() is used to remove an element from the list without index position. The element which needs to 
-be removed from the list should be passed as argument."""
+# 8. remove()
+# remove() removes an element from a list without its index position.
+# The element to be removed from the list should be passed as an argument.
 
 a = [100, 200, 300]
 
 a.remove(200)
 
-print(a)
+print("After removing:", a)
 
-"""
-9. reverse()
-reverse() is used to reverse a list.
-"""
+# 9. reverse()
+# reverse() reverses a list.
 
 a = [50, 60, 70, 80, 90, 100]
 
 a.reverse()
 
-print(a)
+print("After reversing:", a)
+
+# 10. sort()
 
 """
-
-10. sort()
-sort is used to sort a list in ascending order.
-
+sort() is used to sort a list in ascending order by default.
 """
 
 a = [7, 10, 2, 1, 0, 65, 78]
 
 a.sort()
 
-print(a)
+print("Sorted list in ascending order: ", a)
+
+# To sort a list in descending order, we can use the reverse parameter.
+a.sort(reverse=True)
+
+print("Sorted list in descending order: ", a)
